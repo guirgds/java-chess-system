79e5b110c9b6e75acacfb0ec1bf2554693c2b80c
guirgds <guilhermergds@outlook.com>
2024-02-07 18:04:06 -0300

    Switching player each turn

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
       while (true){
          try {
             UI.clearScreen();
-            UI.printBoard(chesMatch.getPieces());
+            UI.printMatch(chesMatch);
             System.out.println();
             System.out.print("Source: ");
             ChessPosition source = UI.readChessPosition(sc);
