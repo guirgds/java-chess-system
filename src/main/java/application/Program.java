909a473297b656e406b40567987a7c04656df6bb
guirgds <guilhermergds@outlook.com>
2024-02-09 16:40:01 -0300

    Fix: protected increaseMoveCount decreseMoveCount

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,53 +1,60 @@
 package application;
 
-import chess.ChessException;
-import chess.ChessMatch;
-import chess.ChessPiece;
-import chess.ChessPosition;
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
+
    public static void main(String[] args) {
+
       Scanner sc = new Scanner(System.in);
-      ChessMatch chesMatch = new ChessMatch();
+      ChessMatch chessMatch = new ChessMatch();
       List<ChessPiece> captured = new ArrayList<>();
 
-      while (!chesMatch.getCheckMate()){
+      while (!chessMatch.getCheckMate()) {
          try {
             UI.clearScreen();
-            UI.printMatch(chesMatch, captured);
+            UI.printMatch(chessMatch, captured);
             System.out.println();
             System.out.print("Source: ");
             ChessPosition source = UI.readChessPosition(sc);
 
-            boolean[][] possibleMoves = chesMatch.possibleMoves(source);
+            boolean[][] possibleMoves = chessMatch.possibleMoves(source);
             UI.clearScreen();
-            UI.printBoard(chesMatch.getPieces(), possibleMoves);
-
+            UI.printBoard(chessMatch.getPieces(), possibleMoves);
             System.out.println();
             System.out.print("Target: ");
             ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chesMatch.performChessMove(source, target);
-            if (captured != null){
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+            if (capturedPiece != null) {
                captured.add(capturedPiece);
             }
+
+            if (chessMatch.getPromoted() != null) {
+               System.out.print("Enter piece for promotion (B/N/R/Q): ");
+               String type = sc.nextLine();
+               chessMatch.replacePromotedPiece(type);
+            }
          }
-         catch (ChessException e){
+         catch (ChessException e) {
             System.out.println(e.getMessage());
             sc.nextLine();
          }
-         catch (InputMismatchException e){
+         catch (InputMismatchException e) {
             System.out.println(e.getMessage());
             sc.nextLine();
+         }
       }
-   }
       UI.clearScreen();
-      UI.printMatch(chesMatch, captured);
-}
+      UI.printMatch(chessMatch, captured);
+   }
 }
--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -5,28 +5,33 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
+
     private Color color;
     private int moveCount;
-
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
     }
-    public ChessPosition getChessPosition() {
-        return ChessPosition.fromPosition(position);
-    }
+
     public Color getColor() {
         return color;
     }
+
     public int getMoveCount() {
         return moveCount;
     }
-    public void increaseMoveCount(){
+
+    protected void increaseMoveCount() {
         moveCount++;
     }
-    public void decreaseMoveCount(){
+
+    protected void decreaseMoveCount() {
         moveCount--;
+    }
+
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(position);
     }
 
     protected boolean isThereOpponentPiece(Position position) {
